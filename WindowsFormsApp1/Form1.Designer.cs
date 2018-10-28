ed154a853760db11e4c7b3a80aa01421b1d4e446
Subodh ARYAL <43497160+asubodh@users.noreply.github.com>
2018-10-28 22:22:15 +0545

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lnkRefresh = new System.Windows.Forms.LinkLabel();
+            this.lnkStop = new System.Windows.Forms.LinkLabel();
+            this.lnkHome = new System.Windows.Forms.LinkLabel();
+            this.txtUrl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.wb1 = new System.Windows.Forms.WebBrowser();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.btnGo);
+            this.panel1.Controls.Add(this.lnkRefresh);
+            this.panel1.Controls.Add(this.lnkStop);
+            this.panel1.Controls.Add(this.lnkHome);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.panel1.Location = new System.Drawing.Point(3, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1342, 80);
+            this.panel1.TabIndex = 0;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1266, 27);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(59, 34);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.UseWaitCursor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lnkRefresh
+            // 
+            this.lnkRefresh.AutoSize = true;
+            this.lnkRefresh.Font = new System.Drawing.Font("Dosis", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkRefresh.LinkColor = System.Drawing.Color.White;
+            this.lnkRefresh.Location = new System.Drawing.Point(93, 33);
+            this.lnkRefresh.Name = "lnkRefresh";
+            this.lnkRefresh.Size = new System.Drawing.Size(69, 21);
+            this.lnkRefresh.TabIndex = 2;
+            this.lnkRefresh.TabStop = true;
+            this.lnkRefresh.Text = "REFRESH";
+            this.lnkRefresh.UseWaitCursor = true;
+            this.lnkRefresh.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRefresh_LinkClicked);
+            // 
+            // lnkStop
+            // 
+            this.lnkStop.AutoSize = true;
+            this.lnkStop.BackColor = System.Drawing.SystemColors.GrayText;
+            this.lnkStop.Font = new System.Drawing.Font("Elemental End", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkStop.LinkColor = System.Drawing.Color.White;
+            this.lnkStop.Location = new System.Drawing.Point(1227, 28);
+            this.lnkStop.Name = "lnkStop";
+            this.lnkStop.Size = new System.Drawing.Size(33, 28);
+            this.lnkStop.TabIndex = 2;
+            this.lnkStop.TabStop = true;
+            this.lnkStop.Text = "X";
+            this.lnkStop.UseWaitCursor = true;
+            this.lnkStop.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // lnkHome
+            // 
+            this.lnkHome.AutoSize = true;
+            this.lnkHome.Font = new System.Drawing.Font("Dosis", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkHome.LinkColor = System.Drawing.Color.White;
+            this.lnkHome.Location = new System.Drawing.Point(29, 33);
+            this.lnkHome.Name = "lnkHome";
+            this.lnkHome.Size = new System.Drawing.Size(47, 21);
+            this.lnkHome.TabIndex = 1;
+            this.lnkHome.TabStop = true;
+            this.lnkHome.Text = "HOME";
+            this.lnkHome.UseWaitCursor = true;
+            this.lnkHome.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.AutoSize = true;
+            this.txtUrl.Font = new System.Drawing.Font("Evogria", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtUrl.Location = new System.Drawing.Point(184, 35);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(89, 19);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "ENTER URL";
+            this.txtUrl.UseWaitCursor = true;
+            this.txtUrl.Click += new System.EventHandler(this.txtURL_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(285, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(944, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.wb1);
+            this.panel2.Location = new System.Drawing.Point(3, 88);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1354, 496);
+            this.panel2.TabIndex = 1;
+            this.panel2.UseWaitCursor = true;
+            // 
+            // wb1
+            // 
+            this.wb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wb1.Location = new System.Drawing.Point(3, 3);
+            this.wb1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb1.Name = "wb1";
+            this.wb1.Size = new System.Drawing.Size(1351, 475);
+            this.wb1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(1232, 456);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "By : Asubodh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1340, 572);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Elemental End", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.Name = "Form1";
+            this.Text = "Go On Browser";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel lnkHome;
+        private System.Windows.Forms.Label txtUrl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel lnkRefresh;
+        private System.Windows.Forms.LinkLabel lnkStop;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser wb1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            wb1.Navigate("https://www.google.com");
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            wb1.Navigate("https://" + textBox1.Text);
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            wb1.Stop();
+        }
+
+        private void txtURL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lnkRefresh_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            wb1.Refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.AcceptButton = btnGo;
+
+
+           
+        }
+    }
+}
